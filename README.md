f2422a033aa56e83ffd1476232f83acd8c56ce1e
Harley Hauer <hhauer@pdx.edu>
2015-07-09 15:12:08 -0700

    Initial commit. POM with Apache Commons FTP client lib. "Hello world" main class.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# FTPanda
+=======
+
+Build with maven.
+Run with: java -cp target\FTPanda-1.0-SNAPSHOT.jar edu.pdx.cs410.FTPanda
---
+++ src/main/java/edu/pdx/cs410/FTPanda.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+/**
+ * Created by Harley on 7/9/2015.
+ */
+public class FTPanda {
+    final private FTPClient ftp = new FTPClient();
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
