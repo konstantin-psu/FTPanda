71c832beae4be7c43b313aa2b34d701abb6f95b4
Konstantin Macarenco <konstan2@pdx.edu>
2015-07-21 19:23:03 -0700

    Started on embeddding simple ftp server

--- src/main/java/edu/pdx/cs410/FTPandaRunner.java
+++ src/main/java/edu/pdx/cs410/FTPandaRunner.java
@@ -1,14 +1,36 @@
 package edu.pdx.cs410;
 
 import org.apache.commons.net.ftp.FTP;
+import org.apache.ftpserver.*;
 
 /**
  * Created by kmacarenco on 7/13/15.
  */
 public class FTPandaRunner {
     public static void main(String[] args) {
-        FTPanda ftpClient = new FTPanda();
-        ftpClient.mainLoop();
+        try {
+            FtpServerFactory serverFactory = new FtpServerFactory();
+            FtpServer server = serverFactory.createServer();
+            // start the server
+            server.start();
+
+            FTPanda ftpClient = new FTPanda();
+            ftpClient.mainLoop();
+        } catch (Exception e) {
+              System.out.println(e.toString());
+        }
+    }
+
+    public void setUser() {
+//        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+//        userManagerFactory.setFile(new File(&quot;myusers.properties&quot;));
+//        userManagerFactory.setPasswordEncryptor(new SaltedPasswordEncryptor());
+//        UserManager um = userManagerFactory.createUserManager();
+//        BaseUser user = new BaseUser();
+//        user.setName(&quot;myNewUser&quot;);
+//        user.setPassword(&quot;secret&quot;);
+//        user.setHomeDirectory(&quot;ftproot&quot;);
+//        um.save(user);
     }
 
 }
